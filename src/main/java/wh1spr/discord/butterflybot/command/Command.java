4221bc8af6acb42b5e5dcebbfc6bf1870c74cc60
Wh1spr <wh1spr.code@gmail.com>
2019-12-17 20:58:54 +0100

    utility methods in command

--- src/main/java/wh1spr/discord/butterflybot/command/Command.java
+++ src/main/java/wh1spr/discord/butterflybot/command/Command.java
@@ -1,11 +1,10 @@
 package wh1spr.discord.butterflybot.command;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.*;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 
+import java.awt.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,6 +13,7 @@ public abstract class Command {
 
     private Set<String> permissions = null;
     private boolean enabled = true;
+    private static final String NO_HELP = "No help was found for this command.";
 
     protected Command(String... permissions) {
         HashSet<String> p = new HashSet<String>();
@@ -44,7 +44,27 @@ public abstract class Command {
         return this.enabled;
     }
 
-    protected String stripCommand(Message m) {
+    protected final String stripCommand(Message m) {
+        if (m.getContentStripped().split(" ").length < 2) return "";
         return m.getContentStripped().split(" ",2)[1];
+    }
+    protected final String getUsedCommand(Message m) {
+        return m.getContentStripped().split(" ",2)[0];
+    }
+
+    //for help command
+    public String getHelpMsg() {
+        return NO_HELP;
+    }
+    public abstract String getUsageMsg();
+
+    protected final void sendIncorrectUse(Message m) {
+        m.getChannel().sendMessage(new EmbedBuilder().setColor(new Color(170, 100, 0))
+                .setTitle(String.format("Usage: `%s %s`", this.getUsedCommand(m), this.getUsageMsg())).build()).queue();
+    }
+
+    protected final void sendFailedMessage(Message m, String message) {
+        m.getChannel().sendMessage(new EmbedBuilder().setColor(new Color(170, 0, 0))
+                .setDescription(message).build()).queue();
     }
 }
