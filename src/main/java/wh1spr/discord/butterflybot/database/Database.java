57ee40613f87278aba9c2d2767f62b16bcdcb518
Wh1spr <wh1spr.code@gmail.com>
2020-01-25 21:19:58 +0100

    Changed db for reserved document creation

--- src/main/java/wh1spr/discord/butterflybot/database/Database.java
+++ src/main/java/wh1spr/discord/butterflybot/database/Database.java
@@ -1,11 +1,14 @@
 package wh1spr.discord.butterflybot.database;
 
+import com.mongodb.MongoSecurityException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import wh1spr.discord.butterflybot.database.entities.users.UserEntity;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Database {
@@ -22,16 +25,24 @@ public class Database {
         return createInstance(url, "ButterflyBot");
     }
     public static Database createInstance(String url, String dbName) {
+        if (INSTANCE != null) throw new IllegalStateException("An instance already exists");
         INSTANCE = new Database(url, dbName);
+        INSTANCE.setupReservedDocuments();
         return INSTANCE;
     }
 
-    private MongoClient client = null;
     private MongoDatabase db = null;
 
-    public Database(String url, String dbName) {
-        this.client = MongoClients.create(url);
-        this.db = this.client.getDatabase(dbName);
+    private Database(String url, String dbName) {
+        MongoClient client = MongoClients.create(url);
+        this.db = client.getDatabase(dbName);
+        try {
+            db.listCollectionNames().first(); //To make sure we dont have auth problems
+        } catch (MongoSecurityException e) {
+            throw new IllegalStateException("Could not authenticate with MongoDB", e);
+        } catch (Exception e) {
+            throw new IllegalStateException("Could not instantiate db connection", e);
+        }
     }
 
     public MongoCollection<Document> getCollection(String collectionName) {
@@ -44,6 +55,15 @@ public class Database {
             if (iter.next().equals(collectionName)) return true;
         }
         return false;
+    }
+
+    private void setupReservedDocuments() {
+        MongoCollection<Document> users = this.getCollection(UserEntity.COLLECTION_NAME);
+        if (!BasicItem.exists(UserEntity.COLLECTION_NAME, 0L)) {
+            Document d = new Document("_id", 0L);
+            d.put("owners", new ArrayList<Long>());
+            users.insertOne(d);
+        }
     }
 
 }
